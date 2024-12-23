aad25be7f2d007c950347a410837735b9e28e788
Faisal Yudha Nugraha <faisalyudha2011@gmail.com>
2024-12-23 08:50:02 +0700

    commti:

--- script/main.js
+++ script/main.js
@@ -40,28 +40,29 @@ const imageBoxes = [
   },
 ];
 
-// const servicesContainer = document.getElementById("services-container");
-// imageBoxes.forEach((box) => {
-//   servicesContainer.innerHTML += `
-//     <div class="flex flex-col justify-between rounded-lg border border-[#262f40] bg-transparent-dark-blue p-4 shadow-lg xl:flex-row xl:items-center xl:p-10">
-//         <div class="flex flex-col xl:mr-8 xl:flex-row">
-//           <img src="${box.image}" alt="${box.title}" class="mb-4 mr-8 w-full rounded-md object-cover xl:mb-0 xl:h-[100px] xl:w-[100px]" />
-//           <div class="flex flex-col justify-center space-y-[8px]">
-//             <h4 class="text-2xl font-semibold leading-relaxed text-white xl:text-[2.5rem]">
-//               ${box.title}
-//             </h4>
-//             <p class="mb-4 text-xl font-medium leading-normal text-gray-custom xl:text-2xl">
-//               ${box.projects}
-//             </p>
-//           </div>
-//         </div>
-//         <a href="${box.link}" class="flex w-full justify-end xl:w-fit">
-//           <img src="assets/arrow-right-alt.svg" alt="arrow-right-alt" />
-//         </a>
-//       </div>
+const servicesContainer = document.getElementById("services-container");
+imageBoxes.forEach((box) => {
+  servicesContainer.innerHTML += `
+    <div class="flex flex-col justify-between rounded-lg border border-[#262f40] bg-transparent-dark-blue p-4 shadow-lg xl:flex-row xl:items-center xl:p-10">
+        <div class="flex flex-col xl:mr-8 xl:flex-row">
+          <img src="${box.image}" alt="${box.title}" class="mb-4 mr-8 w-full rounded-md object-cover xl:mb-0 xl:h-[100px] xl:w-[100px]" />
+          <div class="flex flex-col justify-center space-y-[8px]">
+            <h4 class="text-2xl font-semibold leading-relaxed text-white xl:text-[2.5rem]">
+              ${box.title}
+            </h4>
+            <p class="mb-4 text-xl font-medium leading-normal text-gray-custom xl:text-2xl">
+              ${box.projects}
+            </p>
+          </div>
+        </div>
+        <a href="${box.link}" class="flex w-full justify-end xl:w-fit">
+          <img src="assets/arrow-right-alt.svg" alt="arrow-right-alt" />
+        </a>
+      </div>
 
-//     `;
-// });
+
+    `;
+});
 
 //Porfolio
 const portfolioCards = [
